fbf61a20ffc578d506e83fe476247300bcd7248a
jAxel8 <jaxelsaucedo@gmail.com>
2023-09-18 03:09:30 -0700

    ajsutes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,9 +1,21 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { AlertController, CheckboxChangeEventDetail, CheckboxCustomEvent, DatetimeChangeEventDetail, DatetimeCustomEvent, IonModal } from '@ionic/angular';
+import {
+  AlertController,
+  CheckboxChangeEventDetail,
+  CheckboxCustomEvent,
+  DatetimeChangeEventDetail,
+  DatetimeCustomEvent,
+  IonModal,
+} from '@ionic/angular';
 import { Stats, Task } from '../interfaces/Task';
 import { TaskService } from '../services/task.service';
 import * as moment from 'moment';
-import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
@@ -15,13 +27,17 @@ export class HomePage implements OnInit {
   @ViewChild(IonModal) modal!: IonModal;
   taskForm!: FormGroup;
   loaded = false;
-  skeletons: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+  skeletons: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
   tasks: Task[] = [];
   stats!: Stats;
   public selectedId!: string;
   updateMode = false;
   public selectedDate!: Date;
-  constructor(private taskService: TaskService, private fB: FormBuilder, private alertController: AlertController) { }
+  constructor(
+    private taskService: TaskService,
+    private fB: FormBuilder,
+    private alertController: AlertController
+  ) {}
   ngOnInit(): void {
     this.setupForm();
     this.getTasks();
@@ -31,115 +47,97 @@ export class HomePage implements OnInit {
   setupForm() {
     this.taskForm = this.fB.group({
       task: ['', [Validators.required, Validators.pattern(/^\S.*\S$/)]],
-      date: [new Date(), Validators.required]
-    })
+      date: [new Date(), Validators.required],
+    });
   }
 
   createTask(): void {
-
-
-
-
     this.taskService.create(this.taskForm.value).subscribe(
-      res => {
+      (res) => {
         console.log(res);
         this.modal.dismiss();
 
-
         this.getTasks();
         this.getStats();
-      }, err => {
+      },
+      (err) => {
         console.log(err);
         this.presentAlert('SE PRESENTO UN ERROR AL CREAR LA NUEVA TAREA');
-
-      }, () => {
-        this.taskControl?.reset()
+      },
+      () => {
+        this.taskControl?.reset();
       }
-    )
+    );
   }
-
-
 
   getTasks(): void {
     this.loaded = false;
     this.taskService.get(this.dateControl?.value).subscribe(
-      res => {
-
+      (res) => {
         this.tasks = res;
         this.loaded = true;
-
-
-
-
-      }, err => {
+      },
+      (err) => {
         console.log(err);
       }
-    )
+    );
   }
 
   getStats(): void {
-
     this.taskService.getTaskStats(this.dateControl?.value).subscribe(
-      res => {
-
+      (res) => {
         console.log(res);
 
         this.stats = res;
-
-      }, err => {
+      },
+      (err) => {
         console.log(err);
       }
-    )
+    );
   }
-
 
   update(id: string): void {
     this.taskService.update(this.taskForm.value, id).subscribe(
-      res => {
+      (res) => {
         this.updateMode = false;
         this.getTasks();
         this.presentAlert('TAREA ACTUALIZADA CORRECTAMENTE');
         this.modal.dismiss();
-      }, err => {
+      },
+      (err) => {
         console.log(err);
         this.presentAlert('SE PRESENTO UN ERROR AL ACTUALIZAR ');
-
       }
-    )
+    );
   }
 
   updateStatus(id: string, checked: boolean): void {
     this.taskService.updateDoneStatus(id, checked).subscribe(
-      res => {
-
+      (res) => {
         this.getStats();
-
-      }, err => {
+      },
+      (err) => {
         console.log(err);
-
       }
-    )
+    );
   }
-
-
 
   delete(id: string): void {
     this.taskService.delete(id).subscribe(
-      res => {
+      (res) => {
         this.presentAlert('TAREA ELIMINADA CORRECTAMENTE');
         this.getTasks();
         this.getStats();
-      }, err => {
+      },
+      (err) => {
         console.log(err);
         this.presentAlert('SE PRESENTO UN ERROR, INTENTE DE NUEVO');
-
       }
-    )
+    );
   }
 
   selectDate(e: Event) {
     const ev = e as CustomEvent<DatetimeChangeEventDetail>;
-
 
     // Intenta analizar la cadena de fecha con moment.js
     const parsedDate = moment(ev.detail.value, 'YYYY-MM-DD');
@@ -156,13 +154,8 @@ export class HomePage implements OnInit {
     const ev = e as CustomEvent<CheckboxChangeEventDetail>;
     const checked = ev.detail.checked;
 
-
     this.updateStatus(id, checked);
-
-
-
   }
-
 
   async openUpdateModal(task: Task) {
     this.updateMode = true;
@@ -173,11 +166,8 @@ export class HomePage implements OnInit {
     await this.modal.onWillDismiss().then(() => {
       this.selectedId = '';
       this.taskControl?.reset();
-    })
-
-
+    });
   }
-
 
   //ALERT
 
@@ -190,7 +180,6 @@ export class HomePage implements OnInit {
 
     await alert.present();
   }
-
 
   //GETTERS
   get taskControl() {
